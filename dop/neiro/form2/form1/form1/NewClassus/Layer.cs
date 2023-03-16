a5dfe117717d261f6658b15deb13da3d9420324f
NikitaDemyanenko1922 <90448942+NikitaDemyanenko1922@users.noreply.github.com>
2023-03-16 10:47:10 +0300

    Add files via upload

---
+++ dop/neiro/form2/form1/form1/NewClassus/Layer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace form1.NewClassus
+{
+    class Layer
+    {
+        //поля
+        int non; //num of neuron
+        int nopn; //num of prev neuron
+        //свойства
+
+        //методы
+        //15-70-35-10 логическая функция
+    }
+}
