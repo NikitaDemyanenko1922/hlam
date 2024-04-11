370543dbfcfbd898525a00412d8017ad0229d68a
NikitaDemyanenko1922 <90448942+NikitaDemyanenko1922@users.noreply.github.com>
2024-04-11 23:44:02 +0300

    Add files via upload

---
+++ dop/4kurs/лаб 1 2/Лаб1/Test.m
@@ -0,0 +1,23 @@
+clear; clc;
+x = linspace(-10, 10, 50);
+y(1:length(x))=0;
+for i=1:length(x)
+    y(i) = Test1(x(i));
+end
+figure;
+plot(x,abs(y)/(2*pi),'-b');
+
+hold on;
+y(1:length(x))=0;
+for i=1:length(x)
+    y(i) = Test11(x(i));
+end
+plot(x,abs(y)/(2*pi),'-g');
+
+hold on;
+y1(1:length(x))=0;
+for i=1:length(x)
+    y1(i) = Test2(x(i));
+end
+plot(x,abs(y1),'r--');
+hold off;
